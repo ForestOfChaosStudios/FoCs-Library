f01d5c03dc05076cbd01623a992af8a248107804
JMiles42 <J@jmiles42.com>
2018-02-20 14:10:50 +1100

    Split SerializedObject/Properties Extensions

---
+++ Assets/Plugins/FoCs Lib/Scripts/Editor/Utilities/SerializedObjectExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+
+namespace ForestOfChaosLib.Editor.Utilities
+{
+	public static class SerializedObjectExtensions
+	{
+		public static IEnumerable<SerializedProperty> Properties(this SerializedObject serializedObject, bool enterChildren = false)
+		{
+			var iterator = serializedObject.GetIterator();
+			iterator.Next(true);
+			do
+				yield return iterator.Copy();
+			while(iterator.NextVisible(enterChildren));
+		}
+
+		public static int VisibleProperties(this SerializedObject serializedObject, bool enterChildren = false)
+		{
+			var iterator = serializedObject.GetIterator();
+			iterator.Next(true);
+
+			var num = 0;
+			do
+			{
+				if(!FoCsEditor.IsPropertyHidden(iterator))
+					++num;
+			}
+			while(iterator.NextVisible(enterChildren));
+			return num;
+		}
+	}
+}
---
+++ Assets/Plugins/FoCs Lib/Scripts/Editor/Utilities/SerializedPropertyExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEditor;
+
+namespace ForestOfChaosLib.Editor.Utilities
+{
+	public static class SerializedPropertyExtensions
+	{
+		private const string INDEX_NEEDLE = @"\[[\d]\]";
+
+		public static IEnumerable<SerializedProperty> GetChildren(this SerializedProperty property, bool enterChildren = true)
+		{
+			var iterator = property.Copy();
+			var iteratorNext = property.Next(true);
+
+			if(!iteratorNext)
+				yield break;
+
+			yield return iterator.Copy();
+			do
+			{
+				if(property.depth == iterator.depth)
+					yield break;
+				yield return iterator.Copy();
+			}
+			while(iterator.Next(enterChildren));
+		}
+
+		public static int GetChildrenCount(this SerializedProperty property) => property.GetChildren().Count();
+
+		public static int GetIndex(SerializedProperty property)
+		{
+			var matches = Regex.Matches(property.propertyPath, INDEX_NEEDLE);
+			if(matches.Count == 0)
+				return -1;
+			var str = matches[0].Value.Replace("[", "").Replace("]", "");
+			return int.Parse(str);
+		}
+
+		public static int GetIndex(string path)
+		{
+			var matches = Regex.Matches(path, INDEX_NEEDLE);
+			if(matches.Count == 0)
+				return -1;
+			var str = matches[0].Value.Replace("[", "").Replace("]", "");
+			return int.Parse(str);
+		}
+	}
+}
