c7895209230d1fc2a7180c60d20cac7530573eb9
JMiles42 <J@jmiles42.com>
2018-02-24 21:35:00 +1100

    Finished Limiting Display
    Known bug, extra drag handle under list

---
+++ Assets/Plugins/FoCs Lib/Scripts/Editor/IMUI/FoCsGUIData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ForestOfChaosLib.Editor.ImGUI
+{
+	public class FoCsGUIData : FoCsScriptableObject
+	{
+		public Texture UpArrow;
+		public Texture Up2Arrow;
+		public Texture DownArrow;
+		public Texture Down2Arrow;
+	}
+}
