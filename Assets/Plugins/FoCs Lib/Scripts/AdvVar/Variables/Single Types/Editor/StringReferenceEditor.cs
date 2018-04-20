1fc4c091615ee5f293f997dab6ab8ae0c623d9bd
JMiles42 <J@jmiles42.com>
2018-04-20 11:10:36 +1000

    Added Text Area look to StringReference

---
+++ Assets/Plugins/FoCs Lib/Scripts/AdvVar/Variables/Single Types/Editor/StringReferenceEditor.cs
@@ -0,0 +1,54 @@
+﻿using ForestOfChaosLib.AdvVar;
+using ForestOfChaosLib.Editor;
+using ForestOfChaosLib.Editor.ImGUI;
+using ForestOfChaosLib.Editor.Utilities;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(StringReference))]
+[CanEditMultipleObjects]
+public class StringReferenceEditor: FoCsEditor
+{
+	public override void OnInspectorGUI()
+	{
+		GUIChanged = false;
+		using(EditorDisposables.Indent())
+		{
+			DoDrawHeader();
+			using(var changeCheckScope = EditorDisposables.ChangeCheck())
+			{
+				var cachedGuiColor = GUI.color;
+				serializedObject.Update();
+
+				foreach(var serializedProperty in serializedObject.Properties())
+				{
+					GUI.color = cachedGuiColor;
+					if(serializedProperty.name == "_value")
+					{
+						DoTextBox(serializedProperty);
+					}
+					else
+					{
+						HandleProperty(serializedProperty);
+					}
+				}
+
+				if(changeCheckScope.changed)
+				{
+					serializedObject.ApplyModifiedProperties();
+					GUIChanged = true;
+				}
+			}
+
+			EditorGUILayout.GetControlRect(false, FoCsEditorUtilities.Padding);
+
+			DrawGUI();
+		}
+	}
+
+	private void DoTextBox(SerializedProperty serializedProperty)
+	{
+		EditorGUILayout.LabelField("Value");
+		serializedProperty.stringValue = EditorGUILayout.TextArea(serializedProperty.stringValue,GUILayout.Height(EditorGUIUtility.singleLineHeight * 5));
+	}
+}
