17074d055e2912defb64bff3c5738a228007aa1d
JMiles42 <J@jmiles42.com>
2018-05-15 17:28:19 +1000

    Added UI Texture importer

---
+++ Assets/Plugins/FoCs Lib/Scripts/Editor/Importing/UiITexturePostprocessor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+namespace ForestOfChaosLib.Editor.AssetPostProcessors
+{
+	public class UiITexturePostprocessor: AssetPostprocessor
+	{
+		protected void OnPreprocessTexture()
+		{
+			var textureImporter = (TextureImporter) assetImporter;
+			if(textureImporter.assetPath.Contains("Editor Resources"))
+			{
+				textureImporter.textureType = TextureImporterType.GUI;
+				textureImporter.maxTextureSize = 128;
+			}
+			else if(textureImporter.assetPath.Contains("UI"))
+				textureImporter.textureType = TextureImporterType.Sprite;
+		}
+	}
+}
