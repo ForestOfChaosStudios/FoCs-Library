90c93a454596320c079d89f367f8d98bdc8d78e9
JMiles42 <J@jmiles42.com>
2018-05-23 17:32:47 +1000

    Added Colour & Color AdvVar types, to replace removed Scriptable Objects

---
+++ Assets/Scripts/AdvVar/Variables/List Types/ColorListReference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ForestOfChaosLib.AdvVar.Base;
+using UnityEngine;
+
+namespace ForestOfChaosLib.AdvVar
+{
+	[Serializable]
+	[AdvFolderNameUnityLists]
+	public class ColorListReference: AdvListReference<Color>
+	{ }
+
+	[Serializable]
+	public class ColorListVariable: AdvListVariable<Color, ColorListReference>
+	{ }
+}
---
+++ Assets/Scripts/AdvVar/Variables/List Types/ColourListReference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ForestOfChaosLib.AdvVar.Base;
+using ForestOfChaosLib.Types;
+
+namespace ForestOfChaosLib.AdvVar
+{
+	[Serializable]
+	[AdvFolderNameForestOfChaosLists]
+	public class ColourListReference: AdvListReference<Colour>
+	{ }
+
+	[Serializable]
+	public class ColourListVariable: AdvListVariable<Colour, ColourListReference>
+	{ }
+}
