5f3009e5f5caec9fbf2353184c73294e75309a81
JMiles42 <J@jmiles42.com>
2018-06-05 20:45:12 +1000

    Removed NoFoldoutAttribute from AdvListReference<T>

--- Assets/Scripts/AdvVar/Base/AdvListReference.cs
+++ Assets/Scripts/AdvVar/Base/AdvListReference.cs
@@ -8,10 +8,10 @@ namespace ForestOfChaosLib.AdvVar.Base
 {
 	public class AdvListReference<T>: AdvListReference
 	{
-		[SerializeField] [NoFoldout] private List<T>   _value;
-		[NonSerialized]              public  Action<T> OnValueAdded;
-		[NonSerialized]              public  Action    OnValueChange;
-		[NonSerialized]              public  Action<T> OnValueRemoved;
+		[SerializeField] private List<T>   _value;
+		[NonSerialized]  public  Action<T> OnValueAdded;
+		[NonSerialized]  public  Action    OnValueChange;
+		[NonSerialized]  public  Action<T> OnValueRemoved;
 
 		public List<T> Value
 		{
