333925f46f4dbc5011b1069b9d65530f1314cbb8
JMiles42 <J@jmiles42.com>
2018-06-12 01:45:46 +1000

    Added FoCsAssetFinder.FindAssetsByTypeWithScene()

--- Assets/Scripts/Utilities/FoCsAssetFinder.cs
+++ Assets/Scripts/Utilities/FoCsAssetFinder.cs
@@ -41,5 +41,36 @@ namespace ForestOfChaosLib.Utilities
 			return Resources.FindObjectsOfTypeAll(type);
 #endif
 		}
+		public static T[] FindAssetsByTypeWithScene<T>() where T: Object => FindAssetsByTypeWithScene(typeof(T)).Cast<T>().ToArray();
+
+		public static Object[] FindAssetsByTypeWithScene(Type type)
+		{
+#if UNITY_EDITOR
+			var assets = new List<Object>();
+			var guids  = AssetDatabase.FindAssets($"t:{type.ToString().Replace("UnityEngine.", "")}");
+
+			for(int i = 0; i < guids.Length; i++)
+			{
+				var assetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
+				var subAssets = AssetDatabase.LoadAllAssetsAtPath(assetPath);
+
+				foreach(var subAsset in subAssets)
+				{
+					if(AssetDatabase.IsSubAsset(subAsset))
+					{
+						if(subAsset != null && subAsset.GetType() == type)
+							assets.AddWithDuplicateCheck(subAsset);
+					}
+				}
+			}
+
+			foreach(var o in Object.FindObjectsOfType(type))
+				assets.AddWithDuplicateCheck(o);
+
+			return assets.ToArray();
+#else
+			return Resources.FindObjectsOfTypeAll(type);
+#endif
+		}
 	}
 }
