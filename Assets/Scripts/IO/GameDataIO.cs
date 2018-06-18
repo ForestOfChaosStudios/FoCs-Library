c2ac30934c2db3ccbcca5561c357a1d0e23ef4e9
JMiles42 <J@jmiles42.com>
2018-06-19 00:26:14 +1000

    Started GameDataIO, to save data with BF, and JSON

---
+++ Assets/Scripts/IO/GameDataIO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace ForestOfChaosLib.IO
+{
+	public static class GameDataIO
+	{
+		public const string FILE_EXT = SimpleIO.FILE_EXT;
+
+
+
+
+		public struct GameSaveData
+		{
+			public string Data;
+			public string Hash;
+		}
+	}
+}
