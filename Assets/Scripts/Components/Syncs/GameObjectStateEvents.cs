1470bb69864b6ea3ebfca05f578e3014b8136b34
JMiles42 <J@jmiles42.com>
2018-07-11 23:16:07 +1000

    Added GameObject Active Components
    Allows ONE gameobejct to have the same active state as the other, using events.

---
+++ Assets/Scripts/Components/Syncs/GameObjectStateEvents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ForestOfChaosLib.Extensions;
+
+namespace ForestOfChaosLib.Components.Syncs
+{
+	public class GameObjectStateEvents: FoCsBehaviour
+	{
+		public Action OnEnabled;
+		public Action OnDisabled;
+		public Action OnDestroyed;
+
+        private void OnEnable()
+		{
+			OnEnabled.Trigger();
+        }
+
+		private void OnDisable()
+		{
+			OnDisabled.Trigger();
+        }
+
+		private void OnDestroy()
+		{
+			OnDestroyed.Trigger();
+		}
+	}
+}
---
+++ Assets/Scripts/Components/Syncs/SyncGameObjectActiveState.cs
@@ -0,0 +1,31 @@
+﻿namespace ForestOfChaosLib.Components.Syncs
+{
+	public class SyncGameObjectActiveState: FoCsBehaviour
+	{
+		public GameObjectStateEvents GameObjectToSync;
+
+		private void OnEnable()
+		{
+			GameObjectToSync.OnDisabled += OnDisabled;
+			GameObjectToSync.OnEnabled  += OnEnabled;
+			GameObjectToSync.OnDestroyed  += OnDestroy;
+        }
+
+		private void OnEnabled()
+		{
+			gameObject.SetActive(true);
+		}
+
+		private void OnDisabled()
+		{
+			gameObject.SetActive(false);
+		}
+
+		private void OnDestroy()
+		{
+			GameObjectToSync.OnDisabled  -= OnDisabled;
+			GameObjectToSync.OnEnabled   -= OnEnabled;
+			GameObjectToSync.OnDestroyed -= OnDestroy;
+        }
+	}
+}
