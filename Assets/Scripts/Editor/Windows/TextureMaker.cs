d01932fdd4397a3ace0d2cad317a35d62e234d2e
Unknown <J@jmiles42.com>
2018-07-18 10:38:12 +1000

    Added solid colour texture creator window

---
+++ Assets/Scripts/Editor/Windows/TextureMaker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ForestOfChaosLib.Editor.Windows
+{
+    [FoCsWindow]
+    public class TextureMaker: FoCsWindow<TextureMaker>
+    {
+        private const string TITLE = "Texture Maker";
+
+        [MenuItem(FileStrings.FORESTOFCHAOS_ + TITLE)]
+        private static void Init()
+        {
+            GetWindowAndShow();
+            Window.titleContent.text = TITLE;
+        }
+
+        private Color color;
+
+        protected override void OnGUI()
+        {
+            color = EditorGUILayout.ColorField("Colour To Make Texture", color);
+
+            if(GUILayout.Button("Create Texture"))
+            {
+                var tex = new Texture2D(2, 2);
+                tex.SetPixels(new[] {color, color, color, color});
+                tex.Apply();
+                AssetDatabase.CreateAsset(tex, $"Assets/Solid_{ColorUtility.ToHtmlStringRGB(color)}.asset");
+            }
+        }
+    }
+}
