84033afa415018e638dfbcc57c729801f5d8b254
Unknown <J@jmiles42.com>
2018-08-31 09:20:42 +1000

    Fixed unity UI events issue with simplesceneloader

--- Assets/Scripts/Components/SimpleSceneLoader.cs
+++ Assets/Scripts/Components/SimpleSceneLoader.cs
@@ -6,42 +6,42 @@ namespace ForestOfChaosLib.Components
 	[AddComponentMenu(FoCsStrings.COMPONENTS_FOLDER_ + "Simple Scene Loader")]
 	public class SimpleSceneLoader: FoCsBehaviour
 	{
-		public static void LoadSceneAsync(int s)
+		public void LoadSceneAsync(int s)
 		{
 			SceneManager.LoadSceneAsync(s);
 		}
 
-		public static void LoadSceneAsync(string s)
+		public void LoadSceneAsync(string s)
 		{
 			SceneManager.LoadSceneAsync(s);
 		}
 
-		public static void LoadScene(int s)
+		public void LoadScene(int s)
 		{
 			SceneManager.LoadScene(s);
 		}
 
-		public static void LoadScene(string s)
+		public void LoadScene(string s)
 		{
 			SceneManager.LoadScene(s);
 		}
 
-		public static void LoadSceneAsync(int s, LoadSceneMode mode)
+		public void LoadSceneAsync(int s, LoadSceneMode mode)
 		{
 			SceneManager.LoadSceneAsync(s, mode);
 		}
 
-		public static void LoadSceneAsync(string s, LoadSceneMode mode)
+		public void LoadSceneAsync(string s, LoadSceneMode mode)
 		{
 			SceneManager.LoadSceneAsync(s, mode);
 		}
 
-		public static void LoadScene(int s, LoadSceneMode mode)
+		public void LoadScene(int s, LoadSceneMode mode)
 		{
 			SceneManager.LoadScene(s, mode);
 		}
 
-		public static void LoadScene(string s, LoadSceneMode mode)
+		public void LoadScene(string s, LoadSceneMode mode)
 		{
 			SceneManager.LoadScene(s, mode);
 		}
