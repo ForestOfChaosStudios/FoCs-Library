d63ec14d3f5132cc24aba8f3e6f5e17e2a27cdc1
Jordan Miles <J@jmiles42.com>
2019-05-19 03:04:07 +1000

    Added Dictionary IsNullOrEmpty extension

--- Assets/FoCsLibrary/Scripts/Extensions/DictionaryExtensions.cs
+++ Assets/FoCsLibrary/Scripts/Extensions/DictionaryExtensions.cs
@@ -4,6 +4,8 @@ namespace ForestOfChaosLibrary.Extensions
 {
 	public static class DictionaryExtensions
 	{
+		public static bool IsNullOrEmpty<T, T2>(this Dictionary<T, T2> dictionary) => (dictionary == null) || (dictionary.Count == 0);
+
 		public static void ChangeKey<TKey, TValue>(this IDictionary<TKey, TValue> dic, TKey fromKey, TKey toKey)
 		{
 			var value = dic[fromKey];
