61ad7f8dcd9378b42f60f04ec48caf4fc45e22cc
Jordan Miles <J@jmiles42.com>
2019-05-19 03:07:57 +1000

    Added FoCs Layout Options, to make unity imgui options easier to use

---
+++ Assets/FoCsLibrary/Editor/IMUI/FoCsGUILayoutOptions.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ForestOfChaosLibrary.Editor
+{
+	public static partial class FoCsGUI
+	{
+		public static class LayoutOptions
+		{
+			public struct OptionsArray
+			{
+				public OptionsArray(List<GUILayoutOption>    guiLayout) => _GuiLayout = guiLayout;
+				public OptionsArray(params GUILayoutOption[] guiLayout) => _GuiLayout = new List<GUILayoutOption>(guiLayout);
+				private List<GUILayoutOption> _GuiLayout;
+
+				public OptionsArray AddMoreOptions(GUILayoutOption option)
+				{
+					_GuiLayout.Add(option);
+
+					return this;
+				}
+
+				public OptionsArray AddMoreOptions(GUILayoutOption[] option)
+				{
+					_GuiLayout.AddRange(option);
+
+					return this;
+				}
+
+				public static OptionsArray operator +(OptionsArray a, OptionsArray b)
+				{
+					var rtn = new OptionsArray
+					{
+							_GuiLayout = new List<GUILayoutOption>(a._GuiLayout.Count + b._GuiLayout.Count)
+					};
+
+					rtn._GuiLayout.AddRange(a._GuiLayout);
+					rtn._GuiLayout.AddRange(b._GuiLayout);
+
+					return rtn;
+				}
+
+				public static implicit operator GUILayoutOption[](OptionsArray input) => input._GuiLayout.ToArray();
+
+				public static implicit operator OptionsArray(GUILayoutOption input) =>
+						new OptionsArray
+						{
+								_GuiLayout = new List<GUILayoutOption>
+								{
+										input
+								}
+						};
+
+				public static implicit operator OptionsArray(GUILayoutOption[] input) =>
+						new OptionsArray
+						{
+								_GuiLayout = new List<GUILayoutOption>(input)
+						};
+			}
+
+			public static OptionsArray Expand(bool val = true) => new OptionsArray(GUILayout.ExpandHeight(val), GUILayout.ExpandWidth(val));
+
+#region Height
+			public static OptionsArray ExpandHeight(bool val = true) => GUILayout.ExpandHeight(val);
+			public static OptionsArray SingleLineHeight()            => Height(SingleLine);
+			public static OptionsArray Height(float    val)          => GUILayout.Height(val);
+			public static OptionsArray MinHeight(float val)          => GUILayout.MinHeight(val);
+			public static OptionsArray MaxHeight(float val)          => GUILayout.MaxHeight(val);
+
+			public static OptionsArray ForceHeight(float val) =>
+					new[]
+					{
+							GUILayout.Height(val), GUILayout.MinHeight(val), GUILayout.MaxHeight(val)
+					};
+#endregion
+
+#region Width
+			public static OptionsArray ExpandWidth(bool val = true) => GUILayout.ExpandWidth(val);
+			public static OptionsArray Width(float      val)        => GUILayout.Width(val);
+			public static OptionsArray MinWidth(float   val)        => GUILayout.MinWidth(val);
+			public static OptionsArray MaxWidth(float   val)        => GUILayout.MaxWidth(val);
+
+			public static OptionsArray ForceWidth(float val) =>
+					new[]
+					{
+							GUILayout.Width(val), GUILayout.MinWidth(val), GUILayout.MaxWidth(val)
+					};
+#endregion
+
+		}
+	}
+}
